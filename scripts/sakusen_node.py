221b47827b4c814f84720e2ffd25e53a76e98dae
nesusrobot <kinogakko333@gmail.com>
2022-02-11 11:45:48 +0900

    ロジックを仮で入れてみた

--- scripts/sakusen_node.py
+++ scripts/sakusen_node.py
@@ -3,6 +3,8 @@
 
 # smach
 # https://qiita.com/srs/items/3f5acc64a2faac48b63d
+# https://demura.net/education/lecture/21917.html
+# http://wiki.ros.org/smach/Tutorials
 
 import rospy
 import smach
@@ -15,34 +17,56 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 class AutoRun(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['enemyFound','attackChance'])
+        self.sakuteki_sub = rospy.Subscriber('sakuteki_result', String, self.sakutekiCallback)
+        self.sakutekiResult = ""
+
+    def sakutekiCallback(self, data):
+        self.sakutekiResult = data
 
     def execute(self,userdata):
         rospy.loginfo('Executing state AutoRun')
-        rospy.sleep(2.0)
-        return 'enemyFound'
+        rospy.sleep(1.0)
+
+        if data == "Front":
+            return 'enemyFound'
+        elif data == "Side":
+            return 'attackChance'
 
 
 class EscapeRun(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['attackChance','enemyLost'])
+        self.sakuteki_sub = rospy.Subscriber('sakuteki_result', String, self.sakutekiCallback)
+        self.sakutekiResult = ""
+
+    def sakutekiCallback(self, data):
+        self.sakutekiResult = data
 
     def execute(self,userdata):
         rospy.loginfo('Executing state EscapeRun')
-        return 'attackChance'
+        rospy.sleep(1.0)
+
+        if data == "Front":
+            return 'enemyFound'
+        elif data == "Side":
+            return 'attackChance'
 
 
 class AttackRun(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['enemyLost','enemyFound'])
+        self.sakuteki_sub = rospy.Subscriber('sakuteki_result', String, self.sakutekiCallback)
+        self.sakutekiResult = ""
 
     def execute(self,userdata):
         rospy.loginfo('Executing state AttackRun')
+        rospy.sleep(2.0)
         return 'enemyLost'
 
 
 class SakusenNode():
     def __init__(self):
-        self.sakusen_pub = rospy.Publisher('sakusen_number', Int32, queue_size=10)
+        self.sakusen_pub = rospy.Publisher('sakusen_number', Int32, queue_size=1)
 
         sm_top = smach.StateMachine(outcomes=['succeeded'])
         with sm_top:
